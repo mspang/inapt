59b68f4ac0c344162e84fea9aae3f7c5c4f374bc
Michael Spang <mspang@csclub.uwaterloo.ca>
2009-11-14 19:44:43 -0500

    Use pkgProblemResolver to resolve conflicts
    Signed-off-by: Michael Spang <mspang@csclub.uwaterloo.ca>

--- magic.cc
+++ magic.cc
@@ -6,19 +6,73 @@
 #include <apt-pkg/progress.h>
 #include <apt-pkg/init.h>
 #include <apt-pkg/error.h>
+#include <apt-pkg/algorithms.h>
 
 int main(int argc, char *argv[]) {
 
     pkgInitConfig(*_config);
     pkgInitSystem(*_config, _system);
 
-    OpTextProgress prog;
-    pkgCacheFile cache;
+     _config->Set("Debug::pkgProblemResolver", true);
 
-    if (cache.Open(prog) == false) {
+    OpTextProgress prog;
+    pkgCacheFile cachef;
+
+    if (cachef.Open(prog) == false) {
 	_error->DumpErrors();
         exit(1);
     }
 
-    fprintf(stderr, "%lud\n", cache->BrokenCount());
+    pkgCache *cache = cachef;
+    pkgDepCache *DCache = cachef;
+
+    for (pkgCache::PkgIterator i = cache->PkgBegin(); !i.end(); i++) {
+       if (i.CurrentVer() && !i.CurrentVer().Downloadable()) {
+	       fprintf(stderr, "%s ", i.Name());
+	       fprintf(stderr, "%s\n", DCache->GetCandidateVer(i).VerStr());
+       }
+    }
+
+    DCache->MarkInstall(cache->FindPkg("zsh"), true);
+    DCache->MarkInstall(cache->FindPkg("ssmtp"), true);
+    DCache->MarkInstall(cache->FindPkg("gnome"), true);
+    DCache->MarkInstall(cache->FindPkg("postfix"), true);
+    DCache->MarkDelete(cache->FindPkg("gnome-games"), false);
+
+    fprintf(stderr, "\n");
+    fprintf(stderr, "inst %lu del %lu keep %lu broken %lu bad %lu\n",
+		    DCache->InstCount(), DCache->DelCount(), DCache->KeepCount(),
+		    DCache->BrokenCount(), DCache->BadCount());
+
+    for (pkgCache::PkgIterator i = cache->PkgBegin(); !i.end(); i++) {
+       if ((*DCache)[i].Install())
+         fprintf(stderr, "inst %s\n", i.Name());
+       if ((*DCache)[i].InstBroken())
+         fprintf(stderr, "instbroken %s\n", i.Name());
+       if ((*DCache)[i].NowBroken())
+         fprintf(stderr, "nowbroken %s\n", i.Name());
+    }
+
+    fprintf(stderr, "\n");
+
+    pkgProblemResolver fix (DCache);
+    fix.Protect(cache->FindPkg("ssmtp"));
+    fix.Protect(cache->FindPkg("gnome-games"));
+    fix.Resolve();
+
+    fprintf(stderr, "\n");
+    fprintf(stderr, "inst %lu del %lu keep %lu broken %lu bad %lu\n",
+		    DCache->InstCount(), DCache->DelCount(), DCache->KeepCount(),
+		    DCache->BrokenCount(), DCache->BadCount());
+    for (pkgCache::PkgIterator i = cache->PkgBegin(); !i.end(); i++) {
+       if ((*DCache)[i].Install())
+         fprintf(stderr, "inst %s\n", i.Name());
+       if ((*DCache)[i].InstBroken())
+         fprintf(stderr, "instbroken %s\n", i.Name());
+       if ((*DCache)[i].NowBroken())
+         fprintf(stderr, "nowbroken %s\n", i.Name());
+    }
+
+    fprintf(stderr, "\n");
+
 }
